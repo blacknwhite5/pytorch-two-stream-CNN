cf230836bf89e37f63ce995aa55931af44981886
moohyun <blacknwhite8954@gmail.com>
2018-12-14 12:31:41 +0900

    updated

--- hmdb51.py
+++ hmdb51.py
@@ -18,30 +18,35 @@ class HMDB51(torch.utils.data.Dataset):
         self.train = train
         self.data = {}
         self.onehot_label = {}
+        self.label_index = {}
 
-        video_list = [avi for avi in glob.iglob('**/*.avi', recursive=True)]
         self.labels = sorted(os.listdir(os.path.join(os.getcwd(), 'data/hmdb51_org')))
 
-        # One-hot encoding
-        self.onehot_encoding()
+        video_list = []
+        for label in self.labels:
+            video_list.append([avi for avi in glob.iglob('data/hmdb51_org/{}/*.avi'.format(label), recursive=True)])
+
+        # 레이블 인덱싱
+        self.label_index = {label : np.array(i) for i, label in enumerate(self.labels)}
         
         # 데이터 전처리 (video -> image)
         if not os.path.exists('data/image'):
             for label in self.labels:
                 os.makedirs(os.path.join(os.getcwd(), 'data/image', label), exist_ok=True)
 
-            for video in video_list:
-                self.video2frame(video, os.path.join(os.getcwd(), 'data/image'))
+            for videos in video_list:
+                for i, video in enumerate(videos):
+                    if i < 100:
+                        self.video2frame(video, os.path.join(os.getcwd(), 'data/image'))
+
 
         # {이미지 : 레이블}
         image_list = glob.glob('data/image/**/*.jpg', recursive=True)
         for image in image_list:
-            self.data[image] = self.onehot_label[image.split('/')[-2]]
-
+            self.data[image] = self.label_index[image.split('/')[-2]]
    
-        # train, test split
+        # 학습, 테스트 데이터 나누기
         split_idx = int(len(image_list) * ratio)
-        # random.seed(3)
         random.shuffle(image_list)
         self.train_image, self.test_image = image_list[:split_idx], image_list[split_idx:]
         
@@ -70,15 +75,16 @@ class HMDB51(torch.utils.data.Dataset):
             return len(self.test_image)
 
 
-
-    def onehot_encoding(self):
-        for i, label_name in enumerate(self.labels):
-            onehot = [0] * len(self.labels)
-            onehot[i] = 1
-            self.onehot_label[label_name] = np.array(onehot)
-
-
+    # 동영상에서 프레임 추출
     def video2frame(self, video, save_path, count=0):
+        '''
+            1개의 동영상 파일에서 약 16 프레임씩 이미지(.jpg)로 저장
+
+            args
+                video : 비디오 파일 이름
+                save_path : 저장 경로
+
+        '''
         folder_name, video_name= video.split('/')[-2], video.split('/')[-1]
 
         capture = cv2.VideoCapture(video)
@@ -114,10 +120,10 @@ def main():
                     num_workers=2,
                     shuffle=True)
 
-    for i, (img, label) in enumerate(dataloader):
-        print(img)
-        print(label)
-
+#    for i, (img, label) in enumerate(dataloader):
+#        print(img)
+#        print(label)
+#
 
 
 if __name__ == '__main__':
--- train.py
+++ train.py
@@ -16,9 +16,18 @@ import os
 import numpy as np
 
 
+
+# path
+pretrained = 'data/pretrained'
+params = '/spatial.pth'
+
 # hyper-params
-epochs = 100
+epochs = 10
 batch_size = 1
+lr = 0.001
+momentum = 0.9
+
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
@@ -27,27 +36,33 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 96, kernel_size=7, stride=2),
+            # nn.BatchNorm2d(96),
             nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(3, stride=2),
+            nn.LocalResponseNorm(2),
             nn.Conv2d(96, 256, kernel_size=5, stride=2),
+            # nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(3, stride=2),
+            nn.LocalResponseNorm(2),
             nn.Conv2d(256, 512, kernel_size=3),
             nn.ReLU(),
             nn.Conv2d(512, 512, kernel_size=3),
             nn.ReLU(),
             nn.Conv2d(512, 512, kernel_size=3),
+            # nn.BatchNorm2d(512),            
             nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(3, stride=2),
         )
 
         self.classifier = nn.Sequential(
-            nn.Linear(4608, 4096),
+            nn.Linear(2048, 4096),
             nn.Dropout(),
             nn.Linear(4096, 2048),
             nn.Dropout(),
             nn.Linear(2048, 51),
-            nn.Softmax(dim=1)            
+            # nn.Softmax(dim=1)            
+            
         )
         
 
@@ -58,22 +73,31 @@ class Net(nn.Module):
 
         return x
 
+
+
 # 신경망 구성
 net = Net().to(device)
 print(net)
 
+
+# 신경망 파라매터 로드
+if os.path.isfile(pretrained+params):
+    net.load_state_dict(torch.load(pretrained))
+    print('\n[*]parameters loaded')
+
+
 # loss function, optimizer 정의
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+optimizer = optim.SGD(net.parameters(), lr=lr, momentum=momentum)
 print(optimizer)
 
 
-# 데이터 전처리
+# 데이터 전처리 정의
 transform = transforms.Compose([
          transforms.Resize(255),
          transforms.RandomCrop(224),
          transforms.ToTensor(),
-         # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
          ])
 
 
@@ -98,16 +122,23 @@ def train():
 
             outputs = net(img)
             prediction = torch.max(outputs, 1)[1]
-            loss = criterion(outputs, torch.max(targets, 1)[1])
+
+            loss = criterion(outputs, targets)
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
             if (i+1) % 100 == 0:
-                print('[epoch : {0:3d}, {1:5d}/{2}] loss : {3:3f}'.format(epoch+1, i+1, len(hmdb51), running_loss/(i+1)))
+                print('prediction  : {0} \ntarget      : {1}'.format(prediction, target))
+                print('[epoch : {0:3d}, {1:5d}/{2}] loss : {3:3f}'.format(epoch+1, (i+1)*batch_size, len(hmdb51), running_loss/((i+1)*batch_size)))
     
     print('Training Finished')
 
+
+    # 파라매터 저장
+    if not os.path.exists(pretrained):
+        os.makedirs(pretrained, exist_ok=True)
+    torch.save(net.state_dict(), pretrained+params)
 
 
 def main():
